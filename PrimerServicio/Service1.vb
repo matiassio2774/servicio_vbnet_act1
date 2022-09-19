8592ca65aab0d05dc6af58f9642acb37cb31f059
unknown <siomatias20@gmail.com>
2022-09-19 12:42:23 -0300

    first commit

---
+++ PrimerServicio/Service1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Service1
+    Public TimerServicio As New Timers.Timer
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Agregue el código aquí para iniciar el servicio. Este método debería poner
+        ' en movimiento los elementos para que el servicio pueda funcionar.
+        TimerServicio = New Timers.Timer
+        AddHandler TimerServicio.Elapsed, AddressOf EjecutaUnaAccion
+        TimerServicio.Interval = 6000
+        TimerServicio.Start()
+    End Sub
+
+    Public Function EjecutaUnaAccion()
+        Dim i As Integer
+        For i = 1 To 1000
+            My.Computer.FileSystem.WriteAllText("D:\INFORME.TXT", "LINEA: " & i & vbCrLf,
+           True)
+        Next
+    End Function
+    Protected Overrides Sub OnStop()
+        TimerServicio.Close()
+    End Sub
+    Protected Overrides Sub OnPause()
+        TimerServicio.Stop()
+    End Sub
+
+    Protected Overrides Sub OnContinue()
+        TimerServicio.Start()
+    End Sub
+
+End Class
